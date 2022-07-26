86babd29fad5de7845c830648f053243ff87f655
RobTatarczuch <rob.tatarczuch@gmail.com>
2022-07-26 21:16:41 +0200

    Added maint

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-# Stage-1 Data download and save i JSON file
-# Stage-2 Crypto fear and greed index graph
-# Stage-3 Polkadot and ETH pair value comparrision
-# Stage-4 GUI
+# Stage-1 Data download
+# Stage-2 Cleaning data
+# Stage-3 Crypto fear and greed index graph
+# Stage-4 Data set saving mechanism
+# Stage-5 Polkadot and ETH pair value comparrision
+# Stage-6 GUI
 
 import json
 import requests
-import matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+
+
 # Function definition
 
 # Download data form API
@@ -15,17 +19,19 @@ def download_data():
     data = data_set.json()
     return data
 
-#cleaning data set from usless information
-def sorting_data(data_set):
-    value_list =[]
-    print(type(data_set))
-    value = data_set['data'] #obtaining the main set of data
 
-    #creating new dictionary with most important informations
+# cleaning data set from usless information
+def sorting_data(data_set):
+    value_list = []
+    print(type(data_set))
+    value = data_set['data']  # obtaining the main set of data
+
+    # creating new dictionary with most important informations
     data_dic = {}
-    for i in range(0,len(value)):
+    for i in range(0, len(value)):
         data_dic[data_set['data'][i]['timestamp']] = data_set['data'][i]['value']
     return data_dic
+
 
 # Program sequence
 data_base = sorting_data(download_data())
@@ -35,3 +41,21 @@ with open("Sorted_data.json", "w") as f:
     json.dump(data_base, f)
 
 
+a = [""]
+b = [""]
+for x, y in data_base.items():
+    a.append(str(x))
+    b.append(int(y))
+
+a.pop(0)
+b.pop(0)
+a.reverse()
+b.reverse()
+
+print(a)
+print(b)
+
+plt.plot(a,b)
+plt.title("Fear and Greed index in las mounth",loc= 'left')
+plt.xticks(rotation = 90, fontsize=8)
+plt.show()
